2b392338f78c57809f92a1c453f38ea8e8f6f9cc
Yanbo Liang <ybliang8@gmail.com>
2023-08-28 20:40:53 -0700

    Update

--- test/inductor/test_pattern_matcher.py
+++ test/inductor/test_pattern_matcher.py
@@ -9,6 +9,7 @@ from torch._dynamo.test_case import run_tests, TestCase
 from torch._dynamo.utils import count_calls, counters
 from torch._higher_order_ops.out_dtype import out_dtype
 from torch._inductor.fx_passes import joint_graph
+
 from torch._inductor.fx_passes.serialized_patterns.central_index import (
     get_serialized_pattern,
 )
@@ -17,6 +18,12 @@ from torch._inductor.pattern_matcher import (
     gen_pattern,
     PatternExpr,
     PatternPrettyPrinter,
+    Arg,
+    CallFunction,
+    KeywordArg,
+    Match,
+    PatternMatcherPass,
+    register_graph_pattern,
 )
 from torch._inductor.utils import run_and_get_code
 from torch._inductor.virtualized import V
@@ -783,13 +790,6 @@ class TestPaternMatcher(TestCase):
         counters.clear()
 
     def test_match_with_mutation(self):
-        from torch._inductor.pattern_matcher import (
-            CallFunction,
-            KeywordArg,
-            PatternMatcherPass,
-            register_graph_pattern,
-        )
-
         counter = 0
         test_pass = PatternMatcherPass(prevent_match_across_mutations=True)
 
@@ -956,15 +956,6 @@ class TestPaternMatcher(TestCase):
                 )
 
     def test_match_equivalent_function_invocations1(self):
-        from torch._inductor.pattern_matcher import (
-            Arg,
-            CallFunction,
-            KeywordArg,
-            Match,
-            PatternMatcherPass,
-            register_graph_pattern,
-        )
-
         counter = 0
         test_pass = PatternMatcherPass(prevent_match_across_mutations=True)
 
@@ -978,6 +969,9 @@ class TestPaternMatcher(TestCase):
             return torch.ops.aten.addmm(inp, a, b)
 
         def f1(inp, a, b):
+            return torch.ops.aten.addmm(inp, a, b, beta=1.0)
+
+        def f2(inp, a, b):
             return torch.ops.aten.addmm(inp, a, b, beta=1.0, alpha=1.0)
 
         # This graph pattern should successfully match all of the above functions
@@ -1006,7 +1000,7 @@ class TestPaternMatcher(TestCase):
             "torch._inductor.fx_passes.post_grad.pass_patterns",
             torch._inductor.fx_passes.post_grad.pass_patterns + [test_pass],
         ):
-            for fn in (f0, f1):
+            for fn in (f0, f1, f2):
                 counter = 0
                 expected = fn(*copy.deepcopy(args))
                 opt_fn = torch.compile(fn)
@@ -1018,14 +1012,6 @@ class TestPaternMatcher(TestCase):
                 FileCheck().check_not("extern_kernels.addmm(").run(code[0])
 
     def test_match_equivalent_function_invocations2(self):
-        from torch._inductor.pattern_matcher import (
-            Arg,
-            CallFunction,
-            Match,
-            PatternMatcherPass,
-            register_graph_pattern,
-        )
-
         counter = 0
         test_pass = PatternMatcherPass(prevent_match_across_mutations=True)
 
@@ -1039,6 +1025,9 @@ class TestPaternMatcher(TestCase):
             return torch.ops.aten.addmm(inp, a, b)
 
         def f1(inp, a, b):
+            return torch.ops.aten.addmm(inp, a, b, beta=1.0)
+
+        def f2(inp, a, b):
             return torch.ops.aten.addmm(inp, a, b, beta=1.0, alpha=1.0)
 
         # This graph pattern should only match f0
@@ -1060,12 +1049,58 @@ class TestPaternMatcher(TestCase):
             "torch._inductor.fx_passes.post_grad.pass_patterns",
             torch._inductor.fx_passes.post_grad.pass_patterns + [test_pass],
         ):
-            for fn in (f0, f1):
+            for fn in (f0, f1, f2):
                 counter = 0
                 expected = fn(*copy.deepcopy(args))
                 actual = torch.compile(fn)(*copy.deepcopy(args))
-                # pattern should match
                 self.assertEqual(counter, int(fn is f0))
+                torch.testing.assert_close(actual, expected)
+
+    def test_match_equivalent_function_invocations3(self):
+        counter = 0
+        test_pass = PatternMatcherPass(prevent_match_across_mutations=True)
+
+        args = [
+            torch.randn(20, device="cuda"),
+            torch.randn(10, 15, device="cuda"),
+            torch.randn(15, 20, device="cuda"),
+        ]
+
+        def f0(inp, a, b):
+            return torch.ops.aten.addmm(inp, a, b)
+
+        def f1(inp, a, b):
+            return torch.ops.aten.addmm(inp, a, b, beta=1.0)
+
+        def f2(inp, a, b):
+            return torch.ops.aten.addmm(inp, a, b, beta=1.0, alpha=1.0)
+
+        # This graph pattern should only match f1
+        @register_graph_pattern(
+            CallFunction(
+                torch.ops.aten.addmm, Arg(), Arg(), Arg(), beta=KeywordArg("beta")
+            ),
+            pass_dict=test_pass,
+        )
+        def addmm_replacement(match: Match, inp, mat1, mat2, beta):
+            nonlocal counter
+            counter += 1
+
+            def repl(inp, x1, x2):
+                return x1 @ x2 + inp
+
+            with V.fake_mode:
+                match.replace_by_example(repl, [inp, mat1, mat2])
+
+        with unittest.mock.patch(
+            "torch._inductor.fx_passes.post_grad.pass_patterns",
+            torch._inductor.fx_passes.post_grad.pass_patterns + [test_pass],
+        ):
+            for fn in (f0, f1, f2):
+                counter = 0
+                expected = fn(*copy.deepcopy(args))
+                actual = torch.compile(fn)(*copy.deepcopy(args))
+                self.assertEqual(counter, int(fn is f1))
                 torch.testing.assert_close(actual, expected)
 
 
